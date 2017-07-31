fff1ae78f35fc5768dc732b1c624f43bbfec2593
cyij <cyij2006@gmail.com>
2017-07-31 14:47:18 +0800

    up

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const loaderUtils = require("loader-utils")
 module.exports = function (source) {
   this.cacheable()
   const options = loaderUtils.getOptions(this)
-  if (!options || !options.lang || !options.file) {
+  if (!options || !options.cssFile) {
     return source
   }
-  if (!path.isAbsolute(options.file)) {
-    options.file = path.join(process.cwd(), options.file)
+  options.lang = options.lang || 'less'
+  if (!path.isAbsolute(options.cssFile)) {
+    options.cssFile = path.join(path.dirname(require.main.filename), options.cssFile)
   }
   let styleTagReg = new RegExp('<style[ |\\t]+[^>]*lang=[\'|"]'+options.lang+'[\'|"][^>]*>', 'i')
   let matchs = source.match(styleTagReg)
@@ -31,13 +32,15 @@ module.exports = function (source) {
   }
   for (let i=0; i<importMatches.length; i++) {
     let oneImport = importMatches[i].substr(7).trim()
-    let filePath = path.join(this.context, oneImport.substr(1, oneImport.length-3))
-    if (filePath == options.file) {
+    let cssFilePath = path.join(this.context, oneImport.substr(1, oneImport.length-3))
+    if (cssFilePath == options.cssFile) {
       // file exist
       return source
     }
   }
-  let source0 = source.substr(0, styleStartPos+styleTag.length)
-  let source1 = source.substr(styleStartPos+styleTag.length)
-  return source0 + "\n" + '@import "'+options.file+'";' + "\n" + source1 
+  let lastImport = importMatches[importMatches.length-1]
+  let lastImportStartPos = source.indexOf(lastImport)
+  let source0 = source.substr(0, lastImportStartPos+lastImport.length)
+  let source1 = source.substr(lastImportStartPos+lastImport.length)
+  return source0 + "\n" + '@import "'+options.cssFile+'";' + "\n" + source1 
 }
